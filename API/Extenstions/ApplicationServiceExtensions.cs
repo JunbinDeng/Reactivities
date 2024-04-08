9aba5e41f27cf923230bd033716c120e7edf2bde
zhuimi <zhuimi@pinduoduo.com>
2024-04-08 11:46:09 +0800

    End of section 10

--- API/Extenstions/ApplicationServiceExtensions.cs
+++ API/Extenstions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 using Application.Activities;
 using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -25,6 +27,8 @@ namespace API.Extenstions
             });
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
             services.AddAutoMapper(typeof(MapppingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
 
             return services;
         }
