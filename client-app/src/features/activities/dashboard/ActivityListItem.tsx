3a5864e539a43a607095673d92bd4573ddf7a52c
Junbin Deng <junbin.deng@gmail.com>
2024-12-18 09:50:39 +1300

    End of section 17

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -25,7 +25,7 @@ export default function ActivityListItem({ activity }: Props) {
             <Item.Image
               size='tiny'
               circular
-              src='/assets/user.png'
+              src={activity.host?.image || '/assets/user.png'}
               style={{ marginBottom: 3 }}
             ></Item.Image>
             <Item.Content>
@@ -33,7 +33,10 @@ export default function ActivityListItem({ activity }: Props) {
                 {activity.title}
               </Item.Header>
               <Item.Description>
-                Hosted by {activity.host?.displayName}
+                Hosted by{' '}
+                <Link to={`/profiles/${activity.hostUsername}`}>
+                  {activity.host?.displayName}
+                </Link>
               </Item.Description>
               {activity.isHost && (
                 <Item.Description>
